7ae085b8d2c890cc1febc8914bad0b1908d384b5
Andrea Semprebon <andreasemprebon@gmail.com>
2017-06-03 18:31:14 +0200

    Added unit conversion

--- threads/Publisher.py
+++ threads/Publisher.py
@@ -1,6 +1,9 @@
 import logging
 log = logging.getLogger("root")
 
+from pint import UnitRegistry
+ureg = UnitRegistry()
+
 from threads.AADLThread import AADLThread
 from lxml import etree
 
@@ -17,18 +20,22 @@ class Publisher(AADLThread):
                                             self.tags['TAG_SUBCOMPONENTS'] + "/" +
                                                 self.tags['TAG_SUBCOMPONENT'] + "/" +
                                                     "[" + self.tags['TAG_CATEGORY'] + "='subprogram']");
-        # Source Text
+        ###################
+        ### Source Text ###
+        ###################
         try:
             source_text_property = thread_function.find("./" +
-                                                    self.tags['TAG_PROPERTIES'] + "/" +
-                                                        self.tags['TAG_PROPERTY'] + "/" +
-                                                            "[" + self.tags['TAG_PROPERTY_NAME'] + "='Source_Text']")
+                                                        self.tags['TAG_PROPERTIES'] + "/" +
+                                                            self.tags['TAG_PROPERTY'] + "/" +
+                                                                "[" + self.tags['TAG_PROPERTY_NAME'] + "='Source_Text']")
 
             self.source_text = source_text_property.find(self.tags['TAG_PROPERTY_VALUE']).text
         except AttributeError:
             return (False, "Unable to find property Source_Text");
 
-        # Period
+        ##############
+        ### Period ###
+        ##############
         try:
             period_property = self.thread.find("./" +
                                                 self.tags['TAG_PROPERTIES'] + "/" +
@@ -39,10 +46,18 @@ class Publisher(AADLThread):
         except AttributeError:
             return (False, "Unable to find property Period with relative value and unit");
 
-        log.info("Period: {} {}".format( self.period, self.period_unit) )
+        # Conversione in secondi della frequenza a partire da qualunque unità di misura
+        try:
+            period_quantity = ureg("{} {}".format(self.period, self.period_unit))
+            period_quantity.ito( ureg.second )
+            self.period_in_seconds = period_quantity.magnitude
+        except ValueError:
+            return (False, "Unable to convert Period in seconds");
+
+        log.info("Period: {} {} -> {} s".format( self.period_in_seconds, self.period_unit, self.period_in_seconds) )
         log.info("Source Text: {}".format(self.source_text) )
 
 
-        
+
 
         return (True, "");
